6d93237169ec478863435feb6d89d36ecc61bcb1
Paul Lyon <plyon08@gmail.com>
2017-09-13 13:56:36 -0500

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(event) {
+    const addItemArray = this.state.items;
+    addItemArray.push({text: this._inputElement.value,key: Date.now()});
+    this.setState({items: addItemArray});
+    this._inputElement.value = '';
+    event.preventDefault();
+  }
+
+  deleteItem(event) {
+    let deleteItemArray = this.state.items;
+    const target = event.target.id;
+    deleteItemArray.splice(target,1);
+    this.setState({items: deleteItemArray});
+  }
+
+  render() {
+    const todoEntries = this.state.items;
+    const listItems = todoEntries.map((item,index) => <li key={item.key} id={index} onClick={this.deleteItem}>{item.text}</li>);
+  
+    return (
+      <div className="todoListMain">
+        <div className="header">
+        <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}
+                   placeholder="Enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+        <ul className="theList">{listItems}</ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ToDoList />,
+  document.getElementById('root')
+);
+
